a0f7390cee6f201e9edbadb7405cc255c5b2162f
sainath161 <sainathsmart1234@gmail.com>
2024-04-05 23:41:03 +0530

    last commit

--- src/Components/GoogleLogin.js
+++ src/Components/GoogleLogin.js
@@ -9,7 +9,7 @@ const GoogleLogin = () => {
     <div className="border border-gray-700 w-[460px] rounded-xl h-[312px] flex flex-col items-center text-white bg-gray-850">
       <div className=" flex flex-col py-6 justify-center items-center">
         <h3 className="text-xl pb-4 ">Create a new account</h3>
-        <Link to={"https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-umesh.netlify.app/home"}>
+        <Link to={"https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinboxapp.netlify.app/home"}>
         <button className="border border-gray-500 py-3 px-20 flex justify-center items-center text-base rounded-md">
           <FcGoogle className="text-2xl" />{" "}
           <span className="ml-2">Sign Up with Google</span>
